c7b451372269694c594d490fa4efb52cbc7340c8
arturteben <arturteben@gmail.com>
2026-01-10 22:52:01 -0500

    merge remote changes

---
+++ src/main/java/com/artur/vocab/wordlearningapp/WordLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.artur.vocab.wordlearningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordLearningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordLearningAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artur/vocab/wordlearningapp/WordLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.vocab.wordlearningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordLearningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
